2e861a092c1bb93a0968cb03138b570f58580404
aberamaneza <109916887+aberamaneza@users.noreply.github.com>
2023-10-14 22:56:54 +0200

    Update build.bat

--- build/build.bat
+++ build/build.bat
@@ -2,6 +2,6 @@ cd C:\Users\abderamane\Desktop\opriting system\retros-master\build
 nasm ../src/boot.asm -f bin -o ../build/boot.img
 nasm ../src/kernel.asm -f bin -o ../build/kernel.img
 cd ../build
-copy /y /b boot.img + kernel.img retros.img
+copy /y /b boot.img + kernel.img astraos.img
 
 start boot.bxrc
